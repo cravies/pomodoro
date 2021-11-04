322180691a564adfbf7273766f321ed9bb6bb219
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2021-11-04 15:21:04 +1300

    Fixed bug where timer countdown stacked if you pressed star more than once

--- timer.js
+++ timer.js
@@ -2,7 +2,10 @@
 var mins=25;
 var seconds=0;
 var running=false;
+var num_running=0;
 var default_str="25:00 remaining";
+var looper;
+
 document.getElementById("timer").innerHTML=default_str;
 
 //run the start function every second if called.
@@ -10,6 +13,7 @@ function start_pomo() {
     if (running==false) {
         return 0;
     }
+    
     if (seconds==0) {
         //decrement mins and set seconds to 60
         mins = mins - 1;
@@ -18,7 +22,7 @@ function start_pomo() {
         seconds = seconds - 1;
     }
     document.getElementById("timer").innerHTML=mins+":"+seconds+" remaining";
-    setTimeout(start_pomo,1000);
+    looper = setTimeout(start_pomo,1000);
 }
 
 function pause_pomo() {
@@ -27,8 +31,13 @@ function pause_pomo() {
 }
 
 function set_true() {
-    //allow the clock to run
-    running = true;
+    /*check if the function is already running*/
+    if (running==true) {
+        /*only want one thread at a time*/
+        cleartimeout(looper);
+    } else {
+        running=true;
+    }
 }
 
 function reset_pomo() {
