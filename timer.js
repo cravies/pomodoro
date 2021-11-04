e6ee7f27252217532f910dec4e2094e100c84814
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2021-11-04 14:50:27 +1300

    Fixed a bug in reset function, adding styling for timer border

--- timer.js
+++ timer.js
@@ -17,15 +17,13 @@ function start_pomo() {
     } else {
         seconds = seconds - 1;
     }
-    document.getElementById("timer").innerHTML=mins+":"+seconds+" remaining.";
-    document.getElementById("quote").style.color = "green";
+    document.getElementById("timer").innerHTML=mins+":"+seconds+" remaining";
     setTimeout(start_pomo,1000);
 }
 
 function pause_pomo() {
     //set running = false
     running = false;
-    document.getElementById("quote").style.color = "blue";
 }
 
 function set_true() {
@@ -35,6 +33,9 @@ function set_true() {
 
 function reset_pomo() {
     //reset the time
+    mins=25;
+    seconds=0;
+    running=false;
     var default_str="25:00 remaining";
     document.getElementById("timer").innerHTML=default_str;
 }
