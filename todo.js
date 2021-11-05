70454d887066b193f146902de8cd6c9fc52babe5
meta <pussycrinklage@Cravis-MacBook-Pro.local>
2021-11-05 16:59:35 +1300

    you can now remove elements from todo list by checking them off.

--- todo.js
+++ todo.js
@@ -2,9 +2,25 @@
 function new_task() {
     formdata = sanitize(document.getElementById('newtask').value);
     tasks= document.getElementById('tasks').innerHTML;
-    tasks = tasks + "<input type=\"checkbox\" id=\""+ formdata + ">";
-    tasks = tasks + "<label for=\"" + formdata + "\">" + formdata + "</label><br>";
+    tasks = tasks + "<input class='mycb' type='checkbox' id='"+ formdata + "'>";
+    tasks = tasks + "<label id='"+formdata+"' for='" + formdata + "'>" + formdata + "</label>";
     document.getElementById('tasks').innerHTML = tasks;
+}
+
+function tasks_done() {
+    //iterate over all checkboxes
+    var inputs = document.querySelectorAll('.mycb');
+    //transfer checked elements
+    for (var i=0; i<inputs.length; i++) {
+        if (inputs[i].checked) {
+            //remove label
+            var label_id=inputs[i].id;
+            document.getElementById(label_id).remove();
+            //remove input
+            inputs[i].remove();
+            document.getElementById(label_id).remove();
+        }
+    }
 }
 
 //sanitize user input through encoding
